edce31837e74d2ba9476c760fad64d2e23badfa1
hugo.ferreira <hugo.ferreira@accurate.com>
2021-05-09 16:34:21 -0300

    image

--- models/products.js
+++ models/products.js
@@ -11,6 +11,10 @@ const schema = new mongoose.Schema(
       type: String,
       required: true
     },
+    image: {
+      type: String,
+      required: true
+    },
     description: {
       type: String,
       required: true,
